d49cd5779e4e3eee1ec89c80c32e1b43fcfa35e9
Victor T. Chevalier <VTChevalier@users.noreply.github.com>
2015-08-26 16:05:25 -0500

    Create forecast adapter.

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -73,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
             };
 
             List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+                    // The current context
+                    getActivity(),
+                    // ID of list item layout
+                    R.layout.list_item_forecast,
+                    // ID of the textview to populate
+                    R.id.list_item_forecast_textview,
+                    // Forecast data
+                    weekForecast);
 
             return rootView;
         }
